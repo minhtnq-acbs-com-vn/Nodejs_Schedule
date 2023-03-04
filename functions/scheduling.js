201386cb0abf2fbeac63e46c9d08800a7cb5ff82
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-04 20:57:16 +0700

    logic prototype

--- functions/scheduling.js
+++ functions/scheduling.js
@@ -12,8 +12,6 @@ const GetCronExpress = time => {
   if (time.timeOn.search(":") !== -1) {
     hourOn = time.timeOn.slice(0, time.timeOn.indexOf(":"));
     minuteOn = time.timeOn.slice(time.timeOn.indexOf(":") + 1);
-    console.log(hourOn);
-    console.log(minuteOn);
   }
   //khong tim ra format
   if (time.timeOn.search(":") === -1) {
@@ -24,58 +22,116 @@ const GetCronExpress = time => {
   if (time.timeOff.search(":") !== -1) {
     hourOff = time.timeOff.slice(0, time.timeOff.indexOf(":"));
     minuteOff = time.timeOff.slice(time.timeOff.indexOf(":") + 1);
-    console.log(hourOff);
-    console.log(minuteOff);
   }
   //khong tim ra format
   if (time.timeOff.search(":") === -1) {
     hourOff = time.timeOff;
     minuteOff = "0";
   }
-  // neu co 1 trong 2 time = -1
-  if (time.timeOn === "-1" || time.timeOff === "-1") {
-    if (time.timeOn === "-1") {
-      return `${minuteOff} ${hourOff} * * ${time.dayOfTheWeek}`;
-    }
-    if (time.timeOff === "-1") {
-      return `${minuteOn} ${hourOn} * * ${time.dayOfTheWeek}`;
-    }
+  if (time.timeOn === "-1") {
+    return {
+      type: "singleOff",
+      expression: `${minuteOff} ${hourOff} * * ${time.dayOfTheWeek}`,
+    };
   }
-  // co time on lan off
+  if (time.timeOff === "-1") {
+    return {
+      type: "singleOn",
+      expression: `${minuteOn} ${hourOn} * * ${time.dayOfTheWeek}`,
+    };
+  }
   if (time.timeOn !== "-1" && time.timeOff !== "-1") {
-    
+    return {
+      type: "multiple",
+      expression: [
+        `${minuteOn} ${hourOn} * * ${time.dayOfTheWeek}`,
+        `${minuteOff} ${hourOff} * * ${time.dayOfTheWeek}`,
+      ],
+      order: ["on", "off"],
+    };
   }
-
-  return `0 * * * ${time.dayOfTheWeek}`;
 };
 
-const CreateCronObject = loopTime => {
+const CreateCronObject = (loopTime, req) => {
   let expression = GetCronExpress(loopTime);
-  let valid = cron.validate(expression);
-  if (valid === true) {
-  } else return false;
+  if (expression.type === "singleOn") {
+    if (cron.validate(expression.expression) !== true) return false;
+    RunGoCommand(
+      req.id,
+      "on",
+      `${expression.expression} mosquitto_pub -h localhost -t "${req.topic}" -m "${req.request} -u pi -P Kou-chan1153"`,
+      "create",
+      req.repeat
+    );
+  }
+  if (expression.type === "singleOff") {
+    if (cron.validate(expression.expression) !== true) return false;
+    RunGoCommand(
+      req.id,
+      "off",
+      `${expression.expression} mosquitto_pub -h localhost -t "${req.topic}" -m "${req.request} -u pi -P Kou-chan1153"`,
+      "create",
+      req.repeat
+    );
+  }
+  if (expression.type === "multiple") {
+    let reqArr = [
+      req.request.slice(0, req.request.indexOf(",")),
+      req.request.slice(req.request.indexOf(",") + 1),
+    ];
+    for (let i = 0; i < expression.expression.length; i++) {
+      if (cron.validate(expression.expression[i]) !== true) return false;
+      RunGoCommand(
+        req.id,
+        expression.order[i],
+        `${expression.expression} mosquitto_pub -h localhost -t "${req.topic}" -m "${reqArr[i]} -u pi -P Kou-chan1153"`,
+        "create",
+        req.repeat
+      );
+    }
+  }
 };
 
 const CreateCron = async id => {
   let docID = id.slice(id.indexOf(":") + 1, id.indexOf("-"));
-  let docOp = id.slice(id.indexOf("-") + 1);
-  if (docOp === "delete") {
+  let schedule = await getSchedule(docID);
+  let device = await getRoomDevice(schedule.room);
+
+  if (id.indexOf("(") !== -1) {
+    let docDel = id.slice(id.indexOf("(") + 1, id.indexOf(")"));
+    RunGoCommand(schedule._id, docDel, "", "delete");
+    return;
   }
-  if (docOp === "create" || docOp === "update") {
-    let schedule = await getSchedule(docID);
-    let cronJob = CreateCronObject({
+
+  let cronJob = CreateCronObject(
+    {
       dayOfTheWeek: schedule.dayOfTheWeek,
       timeOn: schedule.timeOn,
       timeOff: schedule.timeOff,
       repeat: schedule.repeat,
-    });
-
-    if (cronJob !== false) {
-      let device = await getRoomDevice(schedule.room);
-      let deviceTopic = device.subscribe;
-      let deviceRequest = schedule.request;
+    },
+    {
+      id: schedule._id,
+      topic: device.subscribe,
+      request: schedule.request,
+      repeat: schedule.repeat,
     }
-    // execSync('cd "C:/Users/potat/AppData/Roaming/BetterDiscord/themes/" && go run main.go')
+  );
+
+  if (cronJob === false) console.log("something wrong with creating cron");
+};
+
+const RunGoCommand = (id, toggle, cronjob, op, repeat) => {
+  if (repeat === "yes") {
+    execSync(
+      `go run main.go -id ${id} -toggle ${toggle} -cronjob ${cronjob} -op ${op}`
+    );
+  }
+  if (repeat === "no") {
+    cronjob += ` && cd /home/pi/Desktop/go && /usr/local/go/bin/go run main.go -id ${id} -toggle ${toggle} -cronjob ${cronjob} -op delete && cd`;
+    execSync(
+      `go run main.go -id ${id} -toggle ${toggle} -cronjob ${cronjob} -op ${op}`
+    );
   }
 };
 
