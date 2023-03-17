1c6b3d4e09e631a39eaff8ae7f8ee7d1efbc6dfe
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-18 01:31:35 +0700

    retry auth jwt

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
+const request = async () => {
+  let response = await axios.post(process.env.requestToken, {
+    email: process.env.email,
+    password: process.env.password,
+  });
+  return response.headers.auth;
+};
+
 const getRoomDevice = async roomName => {
-  let result = await axios.get(process.env.requestRoomDevice + roomName);
+  let token = await request();
+  let result = await axios.get(process.env.requestRoomDevice + roomName, {
+    headers: {
+      auth: token,
+    },
+  });
   let newArr = result.data.map(obj => ({
     deviceModule: obj.deviceModule,
     subscribe: obj.topic.subscribe,
@@ -15,7 +28,12 @@ const getRoomDevice = async roomName => {
 };
 
 const getSchedule = async id => {
-  let result = await axios.get(process.env.requestSchedule + id);
+  let token = await request();
+  let result = await axios.get(process.env.requestSchedule + id, {
+    headers: {
+      auth: token,
+    },
+  });
   return result.data[0];
 };
 
